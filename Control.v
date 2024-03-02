d877863f594e6a6a5e437954b74467eb9c2f3ecb
guardadojr <guardadojr@wisc.edu>
2024-03-02 16:28:59 -0600

    Control module rev0

--- Control.v
+++ Control.v
@@ -8,10 +8,13 @@ module Control(Instruction, RegWrite, ALUSrc,
     output [3:0] Opcode;
 
     assign Opcode = Instruction;
-    
-    // Needed revision
-    // comp. inst. & lw,
-    assign RegWrite = (~Instruction[3] | (Instruction[3] & ~Instruction[0]));
+
+    // comp. inst. & lw,llb,lhb,pcs
+    assign RegWrite = (~Instruction[3] | 
+        (Instruction[3] & ~Instruction[2] & ~Instruction[1] & ~Instruction[0]) | 
+        (Instruction[3] & ~Instruction[2] & Instruction[1] & ~Instruction[0]) |
+        (Instruction[3] & ~Instruction[2] & Instruction[1] & Instruction[0]) | 
+        (Instruction[3] & Instruction[2] & Instruction[1] & ~Instruction[0]));
 
     // sw
     assign MemWrite = (Instruction[3] & ~Instruction[2] & ~Instruction[1] & Instruction[0]);
@@ -22,9 +25,13 @@ module Control(Instruction, RegWrite, ALUSrc,
     // lw
     assign MemtoReg = (Instruction[3] & ~Instruction[2] & ~Instruction[1] & ~Instruction[0]);
 
-    // B
+    // B, Br
     assign Branch = (Instruction[3] & Instruction[2] & ~Instruction[1]);
 
-    assign ALUSrc = ();
+    // SLL, SRA, ROR, LW, SW, LLB, LHB, B
+    // some cases don't matter
+    assign ALUSrc = ((~Instruction[3] & Instruction[2] & ~Instruction[1])|
+        (~Instruction[3] & Instruction[2] & Instruction[1] & ~Instruction[0])|
+        (Instruction[3]));
 
 endmodule
