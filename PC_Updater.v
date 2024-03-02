e2c37b56136822c504b82cc29f2abeb2ba7e45a8
Aydan-WIN10 <aydan.bailey@gmail.com>
2024-03-02 15:32:39 -0800

    fixed compile errors in PC_Updater.v

--- PC_Updater.v
+++ PC_Updater.v
@@ -5,9 +5,9 @@ module PC_Updater(clk, rst, InAddr, cond, Z, N, V, OutAddr);
 	output [15:0] OutAddr;
 
 	wire [15:0] pcOut, pcPlus2, shiftOut, branchAddr, newAddr;
-	wire branch;
+	reg branch;
 
-	Register pc(.clk(clk), .rst(rst), D(newAddr), WriteReg(1'b1), ReadEnable1(1'b1), ReadEnable2(), Bitline1(pc_out), Bitline2());
+	Register pc(.clk(clk), .rst(rst), .D(newAddr), .WriteReg(1'b1), .ReadEnable1(1'b1), .ReadEnable2(), .Bitline1(pc_out), .Bitline2());
 	
 	CLA_16bit plus2(.A(pcOut), .B(16'd2), .Out(pcPlus2));
 	CLA_16bit branchAdd(.A(pcPlus2), .B(shiftOut), .Out(branchAddr));
@@ -15,10 +15,9 @@ module PC_Updater(clk, rst, InAddr, cond, Z, N, V, OutAddr);
 	Shifter ls1(.Shift_Out(shiftOut), .Shift_In(InAddr), .Shift_Val(4'd1), .Mode(1'b0));
 
 	assign newAddr = branch ? newAddr : pcPlus2;
-	assign OutAddr = pc_out;
+	assign OutAddr = pcOut;
 
-	always_comb begin
-		case(cond) begin
+	always @* case(cond)
 		3'b000: begin // Not Equal
 			branch = ~Z ? 1'b1 : 1'b0;
 		end
@@ -36,7 +35,7 @@ module PC_Updater(clk, rst, InAddr, cond, Z, N, V, OutAddr);
 		end
 
 		3'b100: begin // Greater Than or Equal
-			branch = (Z | (~Z & ~N) ? 1'b1 : 1'b0;
+			branch = (Z | (~Z & ~N)) ? 1'b1 : 1'b0;
 		end
 
 		3'b101: begin // Less Than or Equal
@@ -47,12 +46,14 @@ module PC_Updater(clk, rst, InAddr, cond, Z, N, V, OutAddr);
 			branch = V ? 1'b1 : 1'b0;
 		end
 
-		3'111: begin // Unconditional
+		3'b111: begin // Unconditional
 			branch = 1'b1;
 		end
 		
 		// Should never happen
-		default: branch = 1'b0;
-	end
+		default: begin
+			branch = 1'b0;
+		end
+	endcase
 endmodule
 
