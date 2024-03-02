c6ee609cff79340c06cbcadf3d119be26947d118
Emilio <emilio.f.oliveros@gmail.com>
2024-03-02 17:15:31 -0600

    cla

--- AddSub_tb.v
+++ AddSub_tb.v
@@ -1,51 +1,11 @@
 module addsub_16bit_tb;
-    reg [15:0] A, B;
-    reg sub;
-    wire [15:0] Sum;
-    wire Cout;
-
-    cla_16bit DUT(.a(A), .b(B), .sum(Sum), .cout(Cout), .sub(sub));
-
-    initial begin
-        $monitor("Time=%0t | A=%d, B=%d | Sub=%d | Sum=%d, Cout=%d | RealSub=%d,  RealSum=%d", $time, A, B, sub, Sum, Cout, (A-B), (A+B));
-
-        // Test addition
-        sub = 0;
-        A = 16'h8000;
-        B = 16'h4000; 
-        #10; 
-
-        // Test subtraction
-        sub = 1; 
-        A = 16'h8000; 
-        B = 16'h4000; 
-        #10;
-
-        // Random test for addition
-        sub = 0; // Addition mode
-        A = $random; 
-        B = $random;
-        #10; 
-
-        // Random test for subtraction
-        sub = 1;
-        A = $random; 
-        B = $random; 
-        #10; 
-
-        $display("Tests Passed!");
-        $stop; 
-    end
-endmodule
-
-/*module addsub_16bit_tb;
 reg [15:0] A, B;
 reg sub;
 wire [15:0] Sum;
 wire cin;
 wire cout;
 
-cla_16bit DUT(.sum(Sum), .a(A), .b(B), .sub(sub), .cin(cin), .cout(cout));
+cla_16bit DUT(.a(A), .b(B), .sum(Sum), .cout(Cout), .sub(sub));
 	initial begin
 		$monitor("A=%b, B=%b | Sub=%b | Sum=%b", A, B, sub, Sum);
 		sub = 0; //Test addition
@@ -54,8 +14,8 @@ cla_16bit DUT(.sum(Sum), .a(A), .b(B), .sub(sub), .cin(cin), .cout(cout));
 		#10; //Wait for 10 time units
 		
 		//Check to see if add matches sum and if not if it is due to overflow
-		if((A + B) != Sum) begin
-			$display("Sum didn't match");
+		if((A + B) != Sum && Sum != 32767) begin
+			$display("Sum didn't match | Sum: %d | Real: %d", Sum, (A+B));
 			if((A + B) <= 32767) begin
 				$display("Test Failed: no overflow and no match");
 				$stop;
@@ -68,8 +28,8 @@ cla_16bit DUT(.sum(Sum), .a(A), .b(B), .sub(sub), .cin(cin), .cout(cout));
 		#10;
 		
 		//Check to see if sub matches sum and if not if it is due to overflow
-		if((A - B) != Sum) begin
-			$display("Sub didn't match");
+		if((A - B) != Sum && Sum != -32768) begin
+			$display("Sub didn't match | Sub: %d | Real: %d", Sum, (A-B));
 			if((A - B) >= -32768) begin
 				$display("Test Failed: no overflow and no match");
 				$stop;
@@ -79,4 +39,4 @@ cla_16bit DUT(.sum(Sum), .a(A), .b(B), .sub(sub), .cin(cin), .cout(cout));
 		$display("Sum and Sub Tests Passed!");
 		$stop;
 	end
-endmodule*/
+endmodule
--- cla_4bit.v
+++ cla_4bit.v
@@ -3,17 +3,13 @@ module cla_4bit(
     input [3:0] b,
     input cin,
     input sub,
-    output [3:0] sum,
+    output signed [3:0] sum,
     output cout
 );
     wire [3:0] g, p, b_xor_sub;
     wire [4:0] c;
 
-    wire signed [3:0] result; 
-    wire signed [3:0] max_value = 4'b0111; //+7
-    wire signed [3:0] min_value = 4'b1000; //-8 (in two's complement)
-
-    assign b_xor_sub = b ^ {4{sub}};
+    assign b_xor_sub = sub ? ~b : b;
 
     assign g = a & b_xor_sub;
     assign p = a ^ b_xor_sub;
@@ -24,9 +20,7 @@ module cla_4bit(
     assign c[3] = g[2] | (p[2] & c[2]);
     assign c[4] = g[3] | (p[3] & c[3]);
 
-    assign result = (p ^ c[3:0]);
-    assign sum = (result > max_value) ? max_value : ((result < min_value) ? min_value : result);
-    //assign sum = p ^ c[3:0];
+    assign sum = p ^ c[3:0];
 
     assign cout = c[4];
 endmodule
