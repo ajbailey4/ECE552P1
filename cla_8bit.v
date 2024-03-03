b72d313405fb87a8f000ab138fca3807dcf35f5c
Emilio <emilio.f.oliveros@gmail.com>
2024-03-03 10:34:32 -0600

    RED

--- cla_8bit.v
+++ cla_8bit.v
@@ -12,9 +12,8 @@ module cla_16bit(
     cla_4bit cla2(.a(a[7:4]), .b(b[7:4]), .cin(carry[0]), .sub(sub), .sum(result[7:4]), .cout(carry[1]));
 
     //assign sum = result;
-    assign sum = (~result[7] & |result[6:0]) ? 8'h7F :
-        (result[7] & ~&result[6:0]) ? 8'h80 :
-        result[7:0];
-
+    assign sum = (result[7]) ? ((~carry[1]) ? max_value : result):((carry[1]) ? min_value : result);
+    
     assign cout = carry[1];
+
 endmodule
