f248250ddf38f5d64b1b27daa35e625ce942c078
guardadojr <guardadojr@wisc.edu>
2024-03-03 19:58:42 -0600

    hlt

--- Control.v
+++ Control.v
@@ -1,10 +1,10 @@
 // TODO: 3 bits for num of bits. one bit as sined or unsigned out
 
 module Control(Instruction, RegWrite, ALUSrc, 
-    MemWrite, MemtoReg, MemRead, Branch, PCStore, LxB, Br);
+    MemWrite, MemtoReg, MemRead, Branch, PCStore, LxB, Br, hlt);
 
     input [3:0] Instruction;
-    output RegWrite, MemWrite, MemtoReg, MemRead, Branch, ALUSrc, LxB, PCStore, Br; // General Controls
+    output RegWrite, MemWrite, MemtoReg, MemRead, Branch, ALUSrc, LxB, PCStore, Br, hlt; // General Controls
 
     // comp. inst. & lw,llb,lhb,pcs
     assign RegWrite = (~Instruction[3] | 
@@ -37,6 +37,8 @@ module Control(Instruction, RegWrite, ALUSrc,
 
     // Br
     assign Br = Instruction[0];
+
+    assign hlt = &Instruction;
 
 
 endmodule
