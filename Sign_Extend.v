86ce4f6217ac04c928a6580ce353db19acbdefa7
Emilio <emilio.f.oliveros@gmail.com>
2024-03-04 00:42:53 -0600

    signext test

--- Sign_Extend.v
+++ Sign_Extend.v
@@ -3,5 +3,5 @@ module Sign_Extend(Instruction, Out);
 	input [15:0] Instruction;
 	output signed [15:0] Out;
 
-	assign Out = {{16{Instruction[8]}}, Instruction[8:0]};
+	assign Out = Instruction;//{{16{Instruction[8]}}, Instruction[8:0]};
 endmodule
